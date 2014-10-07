9774fecb04b2fcd65b9129b0980810c4cbd76243
JoshuaNotowitz <Joshua@Joshuas-MacBook-Pro.local>
2014-10-07 09:43:52 -0700

    fixeddoubleclick

--- josh_notowitz/app.js
+++ josh_notowitz/app.js
@@ -3,40 +3,40 @@ var letters = []
 
 function clicker() {
 	for (var i = 0; i < table.length; i++) {
-		table[i].addEventListener("click", function(){
+		table[i].addEventListener("click", function add(){
 			console.log(letters.length)
 			letters.push("O")
 			if (letters.length === 0) {
 				this.innerHTML = "X"
-				this.removeEventListener("click", table);
 			}
 			if (letters.length === 1) {
-				this.innerHTML = "0"
+				this.innerHTML = "O"
 			}
 			if (letters.length === 2) {
 				this.innerHTML = "X"
 			}
 			if (letters.length === 3) {
-				this.innerHTML = "0"
+				this.innerHTML = "O"
 			}
 			if (letters.length === 4) {
 				this.innerHTML = "X"
 			}
 			if (letters.length === 5) {
-				this.innerHTML = "0"
+				this.innerHTML = "O"
 			}
 			if (letters.length === 6) {
 				this.innerHTML = "X"
 			}
 			if (letters.length === 7) {
-				this.innerHTML = "0"
+				this.innerHTML = "O"
 			}
 			if (letters.length === 8) {
 				this.innerHTML = "X"
 			}
 			if (letters.length === 9) {
-				this.innerHTML = "0"
+				this.innerHTML = "O"
 			}
+			this.removeEventListener("click", add);
 			console.log(letters.length)
 			console.log(this)
 			
